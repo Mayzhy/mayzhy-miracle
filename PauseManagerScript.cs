973b340baa0f0cfcc140f1b4c19b781ecea62674
Mayzhy <59341946+Mayzhy@users.noreply.github.com>
2020-01-16 18:46:53 +0100

    Add files via upload

---
+++ PauseManagerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManagerScript : MonoBehaviour
+{
+    //permet de faire une scène qui met en pause le jeu
+
+    bool paused = false;
+    public KeyCode pauseButton = KeyCode.Clear;
+    public string pauseScene;
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseButton))
+        {
+            if (paused)
+            {
+                paused = false;
+                Time.timeScale = 1;
+                SceneManager.UnloadSceneAsync(pauseScene);
+
+            }
+            else
+            {
+                paused = true;
+                Time.timeScale = 0;
+                SceneManager.LoadScene(pauseScene, LoadSceneMode.Additive);
+            }
+        }
+    }
+}
+
+
+
+
