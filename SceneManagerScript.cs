f7441d55ab5702991ea7ddfac438d9b389c8df5d
Mayzhy <59341946+Mayzhy@users.noreply.github.com>
2020-01-16 18:48:03 +0100

    Add files via upload

---
+++ SceneManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    //permet de changer de scène grace à des boutons
+
+    public string mainScene;
+    public void ReStartGame()
+    {
+        SceneManager.LoadScene(mainScene);
+    }
+    
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
+
