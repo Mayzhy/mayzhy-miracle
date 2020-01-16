e8f1916325f4379b7472d23565ea9e31139870e4
Mayzhy <59341946+Mayzhy@users.noreply.github.com>
2020-01-16 18:40:28 +0100

    Add files via upload

---
+++ DestroyOnEnter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnEnter : MonoBehaviour
+{
+    
+    //Detruit un objet quand tu passe dessus
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+
+
+}
