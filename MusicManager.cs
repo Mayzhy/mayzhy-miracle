f9932dd6b1a84ab7865802b30f2e57713d3b53b1
Mayzhy <59341946+Mayzhy@users.noreply.github.com>
2020-01-16 18:33:32 +0100

    Add files via upload

---
+++ MusicManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    // Script pour que la musique ne s'arrête pas même si on change de scène
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
