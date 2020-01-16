df49efb1bc6e5f36c75d642a3d626239884c74fd
Mayzhy <59341946+Mayzhy@users.noreply.github.com>
2020-01-16 18:42:13 +0100

    Add files via upload

---
+++ CollisionDialogue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDialogue : MonoBehaviour
+{
+    //Quand tu collide permet d'afficher un dialogue, objet ou autre
+
+    public GameObject Texte;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Player")
+        {
+            Texte.SetActive(true);
+        }
+    }
+}
