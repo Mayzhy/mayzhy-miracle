d9e36fb1dda122f5873fe3e991a09f44326ea5cd
Mayzhy <59341946+Mayzhy@users.noreply.github.com>
2020-01-16 18:38:23 +0100

    Add files via upload

---
+++ DestroyAfterSeconds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSeconds : MonoBehaviour
+{
+    //Objet ce detruit après quelque secondes 
+
+    public float seconds;
+
+   
+    void Start()
+    {
+        GameObject.Destroy(this.gameObject, seconds);
+    }
+
+    
+}
