48a2ac6626a4ee2c625dc4af2f8d39389175ece7
Mayzhy <59341946+Mayzhy@users.noreply.github.com>
2020-01-16 18:36:19 +0100

    Add files via upload

---
+++ SplashScreenManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenManager : MonoBehaviour
+{
+    //Script pour faire un splashscreen
+
+    public float seconds;
+    public string scene;
+
+   
+    void Start()
+    {
+        StartCoroutine(SwitchScene());
+    }
+
+    IEnumerator SwitchScene()
+    {
+
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(scene);
+        //Do the thing
+    }
+
+}
